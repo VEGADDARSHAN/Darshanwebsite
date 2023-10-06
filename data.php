2021d84da1863e0c7852c7ec840901d619166843
VEGADDARSHAN <137590928+VEGADDARSHAN@users.noreply.github.com>
2023-10-06 22:56:18 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,14 +1,15 @@
-<?php
+    <?php
 // Retrieve form data
-$name = $_POST['username'];
-$email = $_POST['password'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$msg= $POST['message']
 
 // Create or open the text file for appending data
 $file = fopen("data.txt", "a");
 
 if ($file) {
     // Format the data and write it to the file
-    $data = "Name: $name, Email: $email\n";
+    $data = "Name: $name, Email: $email, Message: $message\n";
     fwrite($file, $data);
 
     // Close the file
